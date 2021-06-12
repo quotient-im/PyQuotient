42eca39c6f60f089660df2670bf6926f32f6bbaf
Vladyslav Hnatiuk <aders1234@gmail.com>
2021-06-12 09:35:32 +0200

    Fix shiboken command with poetry in build script.

--- build.sh
+++ build.sh
@@ -17,11 +17,12 @@ if [[ ! -z "$QT_PATH" ]]; then
     SHIBOKEN_INCLUDE_ARG="-I$QT_PATH/include/"
 fi
 
+SHIBOKEN_COMMAND="shiboken6"
 if [[ $POETRY = true ]]; then
-    poetry shell
+    SHIBOKEN_COMMAND="poetry run $SHIBOKEN_COMMAND"
 fi
 
-shiboken6  $SHIBOKEN_INCLUDE_ARG \
+$SHIBOKEN_COMMAND  $SHIBOKEN_INCLUDE_ARG \
     --project-file=pyside_typesystem/project.in \
     --typesystem-paths=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")/PySide6/typesystems/ \
     ./pyside_typesystem/bindings.h \
