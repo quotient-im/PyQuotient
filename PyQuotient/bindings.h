0532250bd8fc55c1ca0622ec8fe05b35bd52c166
Vladyslav Hnatiuk <aders1234@gmail.com>
2021-06-17 08:01:31 +0200

    Add option to used prebuilt libQuotient. Base typesystems for BaseJob & related.

--- PyQuotient/bindings.h
+++ PyQuotient/bindings.h
@@ -1,6 +1,13 @@
 #ifndef BINDINGS_H
 #define BINDINGS_H
-#include "libQuotient/lib/jobs/basejob.h"
+// typesystem_qt
+#include <QtCore>/QDebug>
 
+// jobs/typesystem_basejob
+#include "libQuotient/lib/jobs/basejob.h"
+// jobs/typesystem_requestdata
+#include "libQuotient/lib/jobs/requestdata.h"
+
+// typesystem_avatar
 #include "libQuotient/lib/avatar.h"
 #endif // BINDINGS_H
